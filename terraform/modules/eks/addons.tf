b2aad6bc8666f38dbb215612f7f5e8dd85d6f173
Dheeraj Choudhary <dheeraj3choudhary@gmail.com>
2026-03-28 16:51:40 -0400

    add-ons added for eks cluster

---
+++ terraform/modules/eks/addons.tf
@@ -0,0 +1,41 @@
+# EKS Addons - EBS CSI Driver + Default StorageClass + Metrics Server
+
+# -----------------------------
+# EBS CSI Driver Addon
+# -----------------------------
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name      = var.cluster_name
+  addon_name        = "aws-ebs-csi-driver"
+  resolve_conflicts = "OVERWRITE"
+}
+
+# -----------------------------
+# Metrics Server Addon
+# -----------------------------
+resource "aws_eks_addon" "metrics_server" {
+  cluster_name      = var.cluster_name
+  addon_name        = "metrics-server"
+  resolve_conflicts = "OVERWRITE"
+}
+
+# -----------------------------
+# Default StorageClass (gp3)
+# -----------------------------
+resource "kubernetes_storage_class" "gp3" {
+  metadata {
+    name = "gp3"
+
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner = "ebs.csi.aws.com"
+
+  parameters = {
+    type = "gp3"
+  }
+
+  reclaim_policy      = "Delete"
+  volume_binding_mode = "WaitForFirstConsumer"
+}
