64b5e5ac7c5efa5da032dea2a6063951ca4e88ad
Dheeraj Choudhary <dheeraj3choudhary@gmail.com>
2026-03-28 19:28:32 -0400

    add-ons added for eks cluster fix 7

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -2,3 +2,4 @@
 
 output "backend_sa_role_arn"     { value = aws_iam_role.backend_sa.arn }
 output "alb_controller_role_arn" { value = aws_iam_role.alb_controller.arn }
+output "fluent_bit_role_arn" { value = aws_iam_role.fluent_bit.arn }
